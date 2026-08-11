59a15d40faa017b5c50d128ddb4534545bc4911e
LNCracker <loigaak123@gmail.com>
2026-08-12 01:32:24 +0700

    Fix health check connection establishment to respect context deadline

--- pgxpool/pool.go
+++ pgxpool/pool.go
@@ -2,11 +2,14 @@ package pgxpool
 
 import (
 	"context"
+	"net"
+	"net/url"
 	"time"
 )
 
 type ConnConfig struct {
 	ConnectTimeout time.Duration
+	Address        string
 }
 
 type Config struct {
@@ -16,18 +19,56 @@ type Config struct {
 }
 
 type conn struct {
+	netConn net.Conn
 }
 
 func (c *conn) Close(ctx context.Context) error {
+	if c.netConn != nil {
+		return c.netConn.Close()
+	}
 	return nil
 }
 
 type Pool struct {
 	config *Config
+	cancel context.CancelFunc
+}
+
+func ParseConfig(connString string) (*Config, error) {
+	u, err := url.Parse(connString)
+	if err != nil {
+		return nil, err
+	}
+	return &Config{
+		ConnConfig: ConnConfig{
+			Address: u.Host,
+		},
+	}, nil
+}
+
+func ConnectConfig(ctx context.Context, config *Config) (*Pool, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	p := &Pool{
+		config: config,
+		cancel: cancel,
+	}
+	go p.backgroundHealthCheck(ctx)
+	return p, nil
+}
+
+func (p *Pool) Close() {
+	if p.cancel != nil {
+		p.cancel()
+	}
 }
 
 func (p *Pool) connect(ctx context.Context) (*conn, error) {
-	return &conn{}, nil
+	var dialer net.Dialer
+	netConn, err := dialer.DialContext(ctx, "tcp", p.config.ConnConfig.Address)
+	if err != nil {
+		return nil, err
+	}
+	return &conn{netConn: netConn}, nil
 }
 
 func (p *Pool) backgroundHealthCheck(ctx context.Context) {
